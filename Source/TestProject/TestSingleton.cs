c8d35608a76f1f23969905c0dcde9fbd17cbbe1c
Furesoft <filmee24@gmail.com>
2020-02-22 12:43:44 +0100

    cleanup

---
+++ Source/TestProject/TestSingleton.cs
@@ -0,0 +1,13 @@
+﻿using CrossPlattformUtils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestProject
+{
+    [Injectible]
+    public static class TestSingleton
+    {
+        [Injectible(typeof(IEnumerable<int>))]
+        public static IEnumerable<int> MyValue => Enumerable.Range(42, 1);
+    }
+}
--- Source/TestProject/WindowsTerminalMessage.cs
+++ Source/TestProject/WindowsTerminalMessage.cs
@@ -1,5 +1,6 @@
 ﻿using CrossPlattformUtils;
 using NLog;
+using System.Collections.Generic;
 
 namespace TestProject
 {
@@ -8,11 +9,16 @@ namespace TestProject
     {
         public string Message => "im on windows";
 
-        public WindowsTerminalMessage(IOutputWriter output, ILogger logger)
+        public WindowsTerminalMessage(IOutputWriter output, ILogger logger, IEnumerable<int> values)
         {
             this.output = output;
             output.Write("Hello from Windows");
             logger.Debug("all works fine");
+
+            foreach (var v in values)
+            {
+                logger.Info(v);
+            }
         }
 
         private readonly IOutputWriter output;
